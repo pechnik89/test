0750e9081bfa582d7168bd64c37823116be1d3a1
pechnik89 <pechnik89@bk.ru>
2023-01-23 16:14:31 +0500

    test

--- src/Test.js
+++ src/Test.js
@@ -1,13 +1,13 @@
 import { useLayoutEffect } from "react";
 
 const Test = () => {
-	return (
-		<div>
-			<ul>
-				<li>1</li>
-				<li>2</li>
-				<li>3</li>
-			</ul>
-		</div>
-	);
+  return (
+    <div>
+      <ul>
+        <li>1</li>
+        <li>2</li>
+        <li>3</li>
+      </ul>
+    </div>
+  );
 };
