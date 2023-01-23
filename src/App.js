1a6c7b25985c92af612d31e0867abcfa6298cae5
pechnik89 <pechnik89@bk.ru>
2023-01-23 15:53:15 +0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <>
+        <h1>Hello world</h1>
+        <p>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Atque
+          dolores quo voluptate. Consectetur consequatur rerum ducimus ut
+          accusamus? Quos omnis saepe repellendus ab provident quidem, rerum
+          fugit quis minus blanditiis.
+        </p>
+        <form>
+          <div>
+            <input type="checkbox" />
+            <input type="text" />
+          </div>
+          <button>cliock me</button>
+        </form>
+      </>
+    </div>
+  );
+}
+
+export default App;
