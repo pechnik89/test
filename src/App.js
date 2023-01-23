ba6a273f157774bb7ee8ceba4dc31ce8d4b344e1
pechnik89 <pechnik89@bk.ru>
2023-01-23 16:08:22 +0500

    test

--- src/App.js
+++ src/App.js
@@ -6,10 +6,10 @@ function App() {
 			<>
 				<h1>Hello world</h1>
 				<p>
-					Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-					Atque dolores quo voluptate. Consectetur consequatur rerum
-					ducimus ut accusamus? Quos omnis saepe repellendus ab
-					provident quidem, rerum fugit quis minus blanditiis.
+					Lorem ipsum, dolor sit amet consectetur adipisicing elit. Atque
+					dolores quo voluptate. Consectetur consequatur rerum ducimus ut
+					accusamus? Quos omnis saepe repellendus ab provident quidem, rerum
+					fugit quis minus blanditiis.
 				</p>
 				<form>
 					<div>
@@ -22,12 +22,19 @@ function App() {
 						<li>item</li>
 						<li>item</li>
 						<li>item</li>
+
 						<p>Lorem ipsum dolor sit amet.</p>
 						<button>text</button>
 					</ul>
 					<h2>Title</h2>
+
+					<test>Lorem, ipsum dolor.</test>
 				</form>
-				<div></div>
+				<div>1</div>
+				<div>2</div>
+				<div>3</div>
+				<div>4</div>
+				<div>5</div>
 			</>
 		</div>
 	);
