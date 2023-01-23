e6b1d9383ffe5335d7dad4fae637265e1c3934ec
pechnik89 <pechnik89@bk.ru>
2023-01-23 16:05:01 +0500

    test

--- src/App.js
+++ src/App.js
@@ -1,35 +1,36 @@
 import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <>
-        <h1>Hello world</h1>
-        <p>
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Atque
-          dolores quo voluptate. Consectetur consequatur rerum ducimus ut
-          accusamus? Quos omnis saepe repellendus ab provident quidem, rerum
-          fugit quis minus blanditiis.
-        </p>
-        <form>
-          <div>
-            <input type="checkbox" />
-            <input type="text" />
-          </div>
-          <button>cliock me</button>
-          <ul>
-            <li>item</li>
-            <li>item</li>
-            <li>item</li>
-            <li>item</li>
-            <p>Lorem ipsum dolor sit amet.</p>
-            <button>text</button>
-          </ul>
-          <h2>Title</h2>
-        </form>
-      </>
-    </div>
-  );
+	return (
+		<div className="App">
+			<>
+				<h1>Hello world</h1>
+				<p>
+					Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+					Atque dolores quo voluptate. Consectetur consequatur rerum
+					ducimus ut accusamus? Quos omnis saepe repellendus ab
+					provident quidem, rerum fugit quis minus blanditiis.
+				</p>
+				<form>
+					<div>
+						<input type="checkbox" />
+						<input type="text" />
+					</div>
+					<button>cliock me</button>
+					<ul>
+						<li>item</li>
+						<li>item</li>
+						<li>item</li>
+						<li>item</li>
+						<p>Lorem ipsum dolor sit amet.</p>
+						<button>text</button>
+					</ul>
+					<h2>Title</h2>
+				</form>
+				<div></div>
+			</>
+		</div>
+	);
 }
 
 export default App;
